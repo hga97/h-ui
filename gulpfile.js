be1fbd94d612e045c0e79b2f378cee62b8b6501c
huangguian <841428324@qq.com>
2023-02-19 17:05:23 +0800

    feat: 生成css.js，引入css文件

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const babel = require('gulp-babel');
 const less = require('gulp-less');
 const autoprefixer = require('gulp-autoprefixer');
 const cssnano = require('gulp-cssnano');
+const through2 = require('through2');
 
 const paths = {
   dest: {
@@ -14,11 +15,36 @@ const paths = {
   scripts: ['src/**/*.{ts,tsx}', '!src/**/demo/*.{ts,tsx}'], // 脚本文件路径
 };
 
+function cssInjection(content) {
+  return content
+    .replace(/\/style\/?'/g, "/style/css'")
+    .replace(/\/style\/?"/g, '/style/css"')
+    .replace(/\.less/g, '.css');
+}
+
 function compileScripts(babelEnv, destDir) {
   const { scripts } = paths;
   process.env.BABEL_ENV = babelEnv;
 
-  return gulp.src(scripts).pipe(babel()).pipe(gulp.dest(destDir));
+  return gulp
+    .src(scripts)
+    .pipe(babel())
+    .pipe(
+      through2.obj(function z(file, encoding, next) {
+        this.push(file.clone());
+
+        if (file.path.match(/(\/|\\)style(\/|\\)index\.js/)) {
+          const content = file.contents.toString(encoding);
+          file.contents = Buffer.from(cssInjection(content));
+          file.path = file.path.replace(/index\.js/, 'css.js');
+          this.push(file);
+          next();
+        } else {
+          next();
+        }
+      }),
+    )
+    .pipe(gulp.dest(destDir));
 }
 
 function compileCJS() {
